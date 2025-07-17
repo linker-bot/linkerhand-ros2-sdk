0c2695201c0f8403d17768e3a71d36ed01b59607
hejianxin79 <68425458+hejianxin79@users.noreply.github.com>
2025-07-17 11:34:50 +0800

    Update linker_hand_l10_can.py

--- linker_hand_ros2_sdk/linker_hand_ros2_sdk/LinkerHand/core/can/linker_hand_l10_can.py
+++ linker_hand_ros2_sdk/linker_hand_ros2_sdk/LinkerHand/core/can/linker_hand_l10_can.py
@@ -333,11 +333,11 @@ class LinkerHandL10Can:
         return self.thumb_matrix , self.index_matrix , self.middle_matrix , self.ring_matrix , self.little_matrix
     
     def get_matrix_touch_v2(self):
-        self.send_frame(0xb1,[0xc6],sleep=0.003)
-        self.send_frame(0xb2,[0xc6],sleep=0.003)
-        self.send_frame(0xb3,[0xc6],sleep=0.003)
-        self.send_frame(0xb4,[0xc6],sleep=0.003)
-        self.send_frame(0xb5,[0xc6],sleep=0.003)
+        self.send_frame(0xb1,[0xc6],sleep=0.005)
+        self.send_frame(0xb2,[0xc6],sleep=0.005)
+        self.send_frame(0xb3,[0xc6],sleep=0.005)
+        self.send_frame(0xb4,[0xc6],sleep=0.005)
+        self.send_frame(0xb5,[0xc6],sleep=0.005)
         return self.thumb_matrix , self.index_matrix , self.middle_matrix , self.ring_matrix , self.little_matrix
 
     def get_torque(self):
