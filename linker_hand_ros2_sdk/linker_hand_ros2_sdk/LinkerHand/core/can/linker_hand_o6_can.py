88079e6293dfa4ffedca8eff9ddf11d857dbbbf0
hejianxin79 <68425458+hejianxin79@users.noreply.github.com>
2025-09-08 18:04:25 +0800

    Update linker_hand_o6_can.py

--- linker_hand_ros2_sdk/linker_hand_ros2_sdk/LinkerHand/core/can/linker_hand_o6_can.py
+++ linker_hand_ros2_sdk/linker_hand_ros2_sdk/LinkerHand/core/can/linker_hand_o6_can.py
@@ -290,11 +290,11 @@ class LinkerHandO6Can:
         return self.thumb_matrix , self.index_matrix , self.middle_matrix , self.ring_matrix , self.little_matrix
     
     def get_matrix_touch_v2(self):
-        self.send_frame(0xb1,[0xc6],sleep=0.005)
-        self.send_frame(0xb2,[0xc6],sleep=0.005)
-        self.send_frame(0xb3,[0xc6],sleep=0.005)
-        self.send_frame(0xb4,[0xc6],sleep=0.005)
-        self.send_frame(0xb5,[0xc6],sleep=0.005)
+        self.send_frame(0xb1,[0xc6],sleep=0.006)
+        self.send_frame(0xb2,[0xc6],sleep=0.006)
+        self.send_frame(0xb3,[0xc6],sleep=0.006)
+        self.send_frame(0xb4,[0xc6],sleep=0.006)
+        self.send_frame(0xb5,[0xc6],sleep=0.006)
         return self.thumb_matrix , self.index_matrix , self.middle_matrix , self.ring_matrix , self.little_matrix
 
     def get_force(self):
