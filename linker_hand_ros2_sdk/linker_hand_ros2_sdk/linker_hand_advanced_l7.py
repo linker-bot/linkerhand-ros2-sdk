4929873ea8452ea0392f82cb22b393abb0061bc0
hejianxin79 <68425458+hejianxin79@users.noreply.github.com>
2026-02-09 11:02:21 +0800

    Update linker_hand_advanced_l7.py

--- linker_hand_ros2_sdk/linker_hand_ros2_sdk/linker_hand_advanced_l7.py
+++ linker_hand_ros2_sdk/linker_hand_ros2_sdk/linker_hand_advanced_l7.py
@@ -8,10 +8,11 @@ from re import A
 import rclpy,sys                                     # ROS2 Python接口库
 import time
 import argparse
+import numpy as np
 from rclpy.node import Node                      # ROS2 节点类
 from rclpy.clock import Clock
 from std_msgs.msg import String, Header, Float32MultiArray
-from sensor_msgs.msg import JointState
+from sensor_msgs.msg import JointState, PointCloud2, PointField
 import time, json, threading
 from linker_hand_ros2_sdk.LinkerHand.linker_hand_api import LinkerHandApi
 from linker_hand_ros2_sdk.LinkerHand.utils.color_msg import ColorMsg
@@ -45,6 +46,18 @@ class LinkerHandAdvancedL7(Node):
             "ring_matrix":[[-1] * 6 for _ in range(12)],
             "little_matrix":[[-1] * 6 for _ in range(12)]
         }
+        # 压感矩阵合值，单位g 克
+        self.matrix_mass_dic = {
+            "stamp":{
+                "secs": 0,
+                "nsecs": 0,
+            },
+            "thumb_mass":[-1],
+            "index_mass":[-1],
+            "middle_mass":[-1],
+            "ring_mass":[-1],
+            "little_mass":[-1]
+        }
         self.hz = 1.0/60.0
         # ros时间获取
         self.stamp_clock = Clock()
@@ -72,6 +85,8 @@ class LinkerHandAdvancedL7(Node):
             if self.touch_type > 1:
                 ColorMsg(msg=f"{self.hand_type} {self.hand_joint} Equipped with matrix pressure sensing", color='green')
                 self.matrix_touch_pub = self.create_publisher(String, f'/cb_{self.hand_type}_hand_matrix_touch', 10)
+                self.matrix_touch_pub_pc = self.create_publisher(PointCloud2, f'/cb_{self.hand_type}_hand_matrix_touch_pc', 10)
+                self.matrix_touch_mass_pub = self.create_publisher(String, f'/cb_{self.hand_type}_hand_matrix_touch_mass', 10)
             elif self.touch_type != -1:
                 ColorMsg(msg=f"{self.hand_type} {self.hand_joint} Equipped with pressure sensor", color="green")
                 self.touch_pub = self.create_publisher(Float32MultiArray, f'/cb_{self.hand_type}_hand_force', 10)
@@ -168,20 +183,68 @@ class LinkerHandAdvancedL7(Node):
                 self.matrix_dic["ring_matrix"] = self.api.get_ring_matrix_touch(sleep_time=0.006).tolist()
             if self.count == 7:
                 self.matrix_dic["little_matrix"] = self.api.get_little_matrix_touch(sleep_time=0.006).tolist()
-            # 发布压感数据
-            msg = String()
-            current_time = self.stamp_clock.now()
-            # 提取 secs 和 nsecs
-            t_sec = current_time.to_msg().sec
-            t_nanosec = current_time.to_msg().nanosec
-            self.matrix_dic["stamp"]["sec"] = t_sec
-            self.matrix_dic["stamp"]["nanosec"] = t_nanosec
-            msg.data = json.dumps(self.matrix_dic)
-            self.matrix_touch_pub.publish(msg)
+            # 发布矩阵压感数据JSON格式
+            self.pub_matrix_dic()
+            # 发布矩阵压感和值JSON格式
+            self.pub_matrix_mass(dic=self.matrix_dic)
+            # 发布矩阵压感点云格式
+            self.pub_matrix_point_cloud()
         self.count += 1
         if self.count == 8:
             self.count = 0
         time.sleep(0.006)
+
+    def pub_matrix_dic(self):
+        """发布矩阵数据JSON格式"""
+        msg = String()
+        # 获取当前的 ROS 时间
+        current_time = self.stamp_clock.now()
+        # 提取 secs 和 nsecs
+        t_secs = current_time.to_msg().sec
+        t_nsecs = current_time.to_msg().nanosec
+        self.matrix_dic["stamp"]["secs"] = t_secs
+        self.matrix_dic["stamp"]["nsecs"] = t_nsecs
+        msg.data = json.dumps(self.matrix_dic)
+        self.matrix_touch_pub.publish(msg)
+
+    def pub_matrix_mass(self, dic):
+        """发布矩阵数据合值 单位g 克 JSON格式"""
+        msg = String()
+        # 获取当前的 ROS 时间
+        current_time = self.stamp_clock.now()
+        # 提取 secs 和 nsecs
+        t_secs = current_time.to_msg().sec
+        t_nsecs = current_time.to_msg().nanosec
+        self.matrix_mass_dic["stamp"]["secs"] = t_secs
+        self.matrix_mass_dic["stamp"]["nsecs"] = t_nsecs
+        self.matrix_mass_dic["unit"] = "g"
+        self.matrix_mass_dic["thumb_mass"] = sum(sum(row) for row in dic["thumb_matrix"])
+        self.matrix_mass_dic["index_mass"] = sum(sum(row) for row in dic["index_matrix"])
+        self.matrix_mass_dic["middle_mass"] = sum(sum(row) for row in dic["middle_matrix"])
+        self.matrix_mass_dic["ring_mass"] = sum(sum(row) for row in dic["ring_matrix"])
+        self.matrix_mass_dic["little_mass"] = sum(sum(row) for row in dic["little_matrix"])
+        msg.data = json.dumps(self.matrix_mass_dic)
+        self.matrix_touch_mass_pub.publish(msg)
+
+    def pub_matrix_point_cloud(self):
+        tmp_dic = self.matrix_dic.copy()
+        del tmp_dic['stamp']               # 去掉时间戳字段
+        all_matrices = list(tmp_dic.values())  # 5 帧，每帧 6×12=72 个数 or 5 帧，每帧 4×10=40 个数 列x行
+        # 摊平到一维
+        flat_list = [v for frame in all_matrices for v in frame]  
+        flat = np.concatenate([np.asarray(np.clip(c, 0, 255), dtype=np.uint8) for c in flat_list])
+        fields = [PointField(name='val', offset=0, datatype=PointField.UINT8, count=1)]
+        pc = PointCloud2()
+        pc.header.stamp =  self.get_clock().now().to_msg()
+        pc.header.frame_id = ''   # 可改成你需要的坐标系
+        pc.height = 1
+        pc.width = flat.size         # 360
+        pc.fields = fields
+        pc.is_bigendian = False
+        pc.point_step = 1            # 1 个 float32
+        pc.row_step = pc.point_step * pc.width
+        pc.data = flat.tobytes()     # 1440 字节
+        self.matrix_touch_pub_pc.publish(pc)
 
 
     def close_can(self):
